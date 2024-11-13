2f675c4ea71b3db3cdb9d2cdd7ba18dba3d10ae6
Madhvi Asthana <madhviasthana@Madhvis-MacBook-Air.local>
2024-11-14 00:16:33 +0100

    arrow function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { onMounted, ref,onUnmounted } from 'vue';
+import { onMounted, ref, onUnmounted } from 'vue';
 
-import movies from '@/assets/movies.json'
+import movies from '@/assets/movies.json';
 
 import Magnify from 'vue-material-design-icons/Magnify.vue';
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
@@ -14,144 +14,141 @@ import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 import VideoCarousel from '@/components/VideoCarousel.vue';
 import MovieDetails from '@/components/MovieDetails.vue';
 
-import { useMovieStore } from './stores/movie'
+import { useMovieStore } from './stores/movie';
 import { storeToRefs } from 'pinia';
-const useMovie = useMovieStore()
-const { movie, showFullVideo } = storeToRefs(useMovie)
+
+const useMovie = useMovieStore();
+const { movie, showFullVideo } = storeToRefs(useMovie);
+
+const currentSlide = ref(0); // Syncs with the carousel
+let currentMovieIndex = 0; // Tracks the currently selected movie
 
 onMounted(() => {
   console.log('Component mounted, setting initial movie.');
 
-  // Set initial movie after 100ms
+  // Set initial movie and slide
   setTimeout(() => {
     movie.value = movies[0][0];
-    currentSlide.value = 0;  
+    currentSlide.value = 0;
   }, 100);
 
   console.log('Adding keydown event listener.');
-
-  // Add event listener for keydown
   window.addEventListener('keydown', handleKeyPress);
 });
 
 onUnmounted(() => {
-  console.log("Component unmounted, removing keydown event listener.");
+  console.log('Removing keydown event listener.');
   window.removeEventListener('keydown', handleKeyPress);
 });
 
+// Handle keyboard navigation
 function handleKeyPress(event) {
   if (event.key === 'ArrowRight') {
-    // Move the carousel to the right
     moveToNextMovie();
   } else if (event.key === 'ArrowLeft') {
-    // Move the carousel to the left
     moveToPreviousMovie();
   }
 }
 
-let currentMovieIndex = 0;
-
 function moveToNextMovie() {
-  console.log('Moving to the next movie...', currentMovieIndex);
-  const currentCategory = movies[0];
+  const currentCategory = movies[0]; // Assuming first category
   if (currentMovieIndex < currentCategory.length - 1) {
     currentMovieIndex++;
   } else {
-    currentMovieIndex = 0;
+    currentMovieIndex = 0; // Loop back to the start
   }
-  movie.value = currentCategory[currentMovieIndex];
-  currentSlide.value = currentMovieIndex;
-  console.log('Current movie:', movie.value, 'Current slide:', currentSlide.value);
+  updateMovieSelection();
 }
 
-
 function moveToPreviousMovie() {
-  const currentCategory = movies[0];  // Assuming first category for now
+  const currentCategory = movies[0];
   if (currentMovieIndex > 0) {
     currentMovieIndex--;
   } else {
-    currentMovieIndex = currentCategory.length - 1;  // Loop to the last movie
+    currentMovieIndex = currentCategory.length - 1; // Loop to the last movie
   }
-  movie.value = currentCategory[currentMovieIndex];
-  
-
-  // Update the carousel slide
-  currentSlide.value = currentMovieIndex;
+  updateMovieSelection();
 }
 
+function updateMovieSelection() {
+  const currentCategory = movies[0];
+  movie.value = currentCategory[currentMovieIndex];
+  currentSlide.value = currentMovieIndex; // Sync with the carousel
+  console.log('Current movie:', movie.value, 'Current slide:', currentSlide.value);
+}
 
+// Sync the slide index with carousel events
 const updateMovieIndex = (newIndex) => {
   currentMovieIndex = newIndex;
+  movie.value = movies[0][currentMovieIndex]; // Update movie
 };
-
-
-
-console.log('Selected movie:', movie.value);
-
-console.log('Adding keydown event listener.');
-window.addEventListener('keydown', handleKeyPress);
-
 </script>
 
 <template>
- 
- <div class="fixed w-full h-screen bg-black">
-
-<div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
-  <img class="absolute top-0 w-[35px] mt-10 ml-10" src="/images/netflix-logo.png">
-  <div>
-    <div class="py-2 mx-10 my-6">
-      <Magnify fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+  <div class="fixed w-full h-screen bg-black">
+    <div v-if="!showFullVideo" id="SideNav" class="flex z-40 items-center w-[120px] h-screen bg-black relative">
+      <img class="absolute top-0 w-[35px] mt-10 ml-10" src="/images/netflix-logo.png">
+      <div>
+        <div class="py-2 mx-10 my-6">
+          <Magnify fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Television fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <MovieOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Plus fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+        </div>
+      </div>
     </div>
-    <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
-      <HomeOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
+    <div v-if="!showFullVideo">
+      <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
+        <div class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black" />
+
+        <MovieDetails v-if="movie" :movie="movie"/>
+
+        <video
+          v-if="movie"
+          :src="'/videos/'+movie.name+'.mp4'"
+          autoplay
+          loop
+          class="absolute z-0 h-[600px] right-0 top-0"
+        />
+      </div>
+
+      <div class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto">
+        <!-- Pass currentSlide and update logic to VideoCarousel -->
+        <VideoCarousel 
+          class="pb-14 pt-14" 
+          category="Popular Movies" 
+          :movies="movies[0]" 
+          :currentMovieIndex="currentSlide" 
+          @updateSlide="updateMovieIndex" 
+        />
+        <VideoCarousel 
+          class="pb-14 pt-14" 
+          category="Horror Movies" 
+          :movies="movies[1]" 
+          :currentMovieIndex="currentSlide" 
+          @updateSlide="updateMovieIndex" 
+        />
+        <VideoCarousel 
+          class="pb-32 pt-14" 
+          category="Featured Movies" 
+          :movies="movies[2]" 
+          :currentMovieIndex="currentSlide" 
+          @updateSlide="updateMovieIndex" 
+        />
+      </div>
     </div>
-    <div class="py-2 mx-10 my-6">
-      <TrendingUp fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
-    </div>
-    <div class="py-2 mx-10 my-6">
-      <Television fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
-    </div>
-    <div class="py-2 mx-10 my-6">
-      <MovieOutline fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
-    </div>
-    <div class="py-2 mx-10 my-6">
-      <Plus fillColor="#FFFFFF" :size="40" class="cursor-pointer"/>
-    </div>
-    </div>
-  </div>
-  <div v-if="!showFullVideo">
-    <div class="fixed flex z-20 top-0 right-0 w-full h-[50%] bg-black pl-[120px] bg-clip-border">
-      <div class="absolute z-30 h-[600px] left-[120px] w-[77%] right-0 top-0 bg-gradient-to-r from-black via-black" />
-
-      <MovieDetails v-if="movie" :movie="movie"/>
-
-
-      <video
-v-if="movie"
-:src="'/videos/'+movie.name+'.mp4'"
-autoplay
-loop
-class="absolute z-0 h-[600px] right-0 top-0"
-
-/>
-<div class="fixed z-30 bottom-0 right-0 w-full h-[55%] pl-[120px] overflow-y-auto">
-<VideoCarousel class="pb-14 pt-14" category="Poplular Movies" :movies="movies[0]"   :currentMovieIndex="currentMovieIndex" @updateSlide="updateMovieIndex"  />
-<VideoCarousel class="pb-14 pt-14" category="Horror Movies" :movies="movies[1]":currentMovieIndex="currentMovieIndex" @updateSlide="updateMovieIndex" />
-<VideoCarousel class="pb-32 pt-14" category="Featured Movies" :movies="movies[2]" :currentMovieIndex="currentMovieIndex" @updateSlide="updateMovieIndex"/>
-
-
-
-
-</div>
-
-    </div>
-
-
-
-  </div>
-    </div>
-
     <div v-if="showFullVideo">
       <div @click="showFullVideo = false" class="absolute z-50 p-2 m-4 bg-white bg-opacity-50 rounded-full cursor-pointer">
         <ChevronLeft fillColor="#FFFFFF" :size="40"/>
@@ -164,6 +161,5 @@ class="absolute z-0 h-[600px] right-0 top-0"
         class="absolute z-0 w-[100vw] h-full object-fit"
       />
     </div>
-
+  </div>
 </template>
-
--- src/components/VideoCarousel.vue
+++ src/components/VideoCarousel.vue
@@ -6,19 +6,13 @@
             </div>
         </div>
         <div v-for="(movie, index) in movies" :key="index" :class="{ 'active': index === currentMovieIndex }">
-  <!-- Movie content -->
-</div>
-
-
-
-
+        <!-- Movie content -->
+        </div>
 
         <Carousel 
-        ref="carousel"
-    :modelValue="currentSlide"
-    @update:modelValue="updateSlideValue"
-
-
+            ref="carousel"
+            :modelValue="currentSlide"
+            @update:modelValue="updateSlideValue"
             :items-to-show="8" 
             :items-to-scroll="1"
             :wrap-around="true"
@@ -26,8 +20,11 @@
             snapAlign="start"
             class="bg-transparent"
         >
-        <Slide v-for="(slide, index) in movies" :key="slide.name" class="flex items-center object-cover text-white bg-transparent">
-
+            <Slide 
+                v-for="(slide, index) in movies" 
+                :key="slide.name" 
+                class="flex items-center object-cover text-white bg-transparent"
+            >
                 <div 
                     @click="fullScreenVideo(index)"
                     class="object-cover h-[100%] hover:brightness-125 cursor-pointer" 
@@ -51,49 +48,65 @@
 </template>
 
 <script setup>
-  import { ref, toRefs } from 'vue'
-  import 'vue3-carousel/dist/carousel.css'
-  import { Carousel, Slide, Navigation } from 'vue3-carousel'
+import { ref, toRefs, onMounted, onUnmounted } from 'vue';
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+import { useMovieStore } from '../stores/movie';
+import { storeToRefs } from 'pinia';
 
-  import { useMovieStore } from '../stores/movie'
-  import { storeToRefs } from 'pinia';
-  const useMovie = useMovieStore()
-  const { movie, showFullVideo } = storeToRefs(useMovie)
+const useMovie = useMovieStore();
+const { movie, showFullVideo } = storeToRefs(useMovie);
 
-  let currentSlide = ref(0)
+let currentSlide = ref(0);
 
-  const props = defineProps({ category: String, movies: Array })
-  const { movies, category } = toRefs(props)
-  const emit = defineEmits(['updateSlide']);
+const props = defineProps({ category: String, movies: Array });
+const { movies, category } = toRefs(props);
+const emit = defineEmits(['updateSlide']);
 
-  const currentSlideObject = (slide, index) => {
+const currentSlideObject = (slide, index) => {
     if (index === currentSlide.value) {
-        movie.value = slide
+        movie.value = slide;
     }
-  }
-
-  const fullScreenVideo = (index) => {
-    currentSlide.value = index
-    setTimeout(() => showFullVideo.value = true, 500)
-  }
-  const updateSlideValue = (val) => {
-  currentSlide.value = val;
-  emit('updateSlide', val);  // Emit slide change to the parent
 };
 
-  
+const fullScreenVideo = (index) => {
+    currentSlide.value = index;
+    setTimeout(() => (showFullVideo.value = true), 500);
+};
+
+const updateSlideValue = (val) => {
+    currentSlide.value = val;
+    emit('updateSlide', val); // Emit slide change to the parent
+};
+
+// Handle keyboard navigation
+const handleKeyboardNavigation = (event) => {
+    if (event.key === 'ArrowRight') {
+        currentSlide.value = (currentSlide.value + 1) % movies.value.length; // Move to next slide
+    } else if (event.key === 'ArrowLeft') {
+        currentSlide.value = (currentSlide.value - 1 + movies.value.length) % movies.value.length; // Move to previous slide
+    }
+};
+
+// Attach and remove event listeners
+onMounted(() => {
+    window.addEventListener('keydown', handleKeyboardNavigation);
+});
+onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeyboardNavigation);
+});
 </script>
 
 <style>
-    .carousel__prev, 
-    .carousel__next, 
-    .carousel__prev:hover, 
-    .carousel__next:hover {
-        color: white;
-    }
-.active {
-  border: 4px solid white;  /* Ensure this visually highlights the selected movie */
+.carousel__prev, 
+.carousel__next, 
+.carousel__prev:hover, 
+.carousel__next:hover {
+    color: white;
 }
 
-
+.active {
+    border: 4px solid white; /* Visually highlight the selected movie */
+    transition: border-color 0.3s ease; /* Smooth transition for better UX */
+}
 </style>
